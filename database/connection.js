b0b7f08d9131cf82aa64eb82b025ae6d7421f448
Jin Cai <jinruncai@gmail.com>
2020-05-16 16:22:30 -0700

    Create connection file to cosmosdb

---
+++ database/connection.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const {
+  COSMOSDB_HOST,
+  COSMOSDB_PORT,
+  COSMOSDB_DBNAME,
+  COSMOSDB_USER,
+  COSMOSDB_PASS,
+} = process.env;
+
+
+const connect = (callback = () => {}) => {
+  mongoose.connect(
+    `mongodb://${COSMOSDB_HOST}:${COSMOSDB_PORT}/${COSMOSDB_DBNAME}?ssl=true&replicaSet=globaldb`,
+    {
+      auth: {
+        user: COSMOSDB_USER,
+        password: COSMOSDB_PASS,
+      },
+    },
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('connected to database');
+        callback();
+      }
+    },
+  );
+};
+
+module.exports = connect;
