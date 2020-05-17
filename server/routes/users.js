f1e3bd4251052c7d07d81cb975eb1521cbd2778e
Jin Cai <jinruncai@gmail.com>
2020-05-17 00:29:44 -0700

    Add user routes
    Get user info
    Create new users
    Search for users by text matching

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,13 +2,70 @@ const express = require('express');
 
 const router = express.Router();
 
-const { User } = require('../../database/index');
+const { User, Thread } = require('../../database/index');
 
-// Get all information for a user
-router.get('/:userId', (req, res) => {
-  const filter = { userId: req.params };
-
-  User.find(filter)
-    .then((user) => res.send(user))
+router.get('/search', (req, res) => {
+  const {
+    searchText,
+  } = req.query;
+  console.log(searchText);
+  User.find({ username: new RegExp(searchText, 'i') }, '_id, username', { limit: 10 })
+    .then((users) => res.status(200).send(users))
+    .catch((err) => res.status(400).send('Error searching usernames', err));
 });
 
+router.get('/:userId', (req, res) => {
+  const { userId } = req.params;
+  const projection = '-password';
+  User.findById(userId, projection)
+    .then((user) => res.send(user));
+});
+
+router.post('/', (req, res) => {
+  const {
+    username,
+    password,
+    email,
+    refererId,
+  } = req.body;
+  User.find({ username })
+    .then((user) => {
+      if (user.length) {
+        throw new Error('Username already taken');
+      }
+    })
+    .then(() => {
+      const newUser = new User({
+        username,
+        password,
+        email,
+      });
+      return newUser.save();
+    })
+    .then(({ _id }) => {
+      if (refererId) {
+        const thread = new Thread({
+          userIds: [_id, refererId],
+          messages: [{
+            senderId: refererId,
+            recipientId: _id,
+            seen: false,
+            resource: 'seed',
+            resourceClaimed: false,
+            type: 'referral',
+          }],
+        });
+        return thread.save();
+      }
+      return null;
+    })
+    .then(() => {
+      res.status(201).send();
+    })
+    .catch((err) => {
+      console.log(err.toString());
+      res.status(400).send(err.toString());
+    });
+});
+
+module.exports = router;
