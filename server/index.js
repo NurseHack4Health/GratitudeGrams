6a87a960add8693eef86be20f59e1482afd12301
Jin Cai <jinruncai@gmail.com>
2020-05-16 18:17:14 -0700

    Add body parser and connect database to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require('../database/index');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-// eslint-disable-next-line no-console
-app.listen(PORT, () => console.log('Server listening on port', PORT));
+app.use(bodyParser.json());
+
+db.connect(() => {
+  app.listen(PORT, () => console.log('Server listening on port', PORT));
+});
